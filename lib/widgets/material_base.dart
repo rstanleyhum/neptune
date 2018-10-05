8d6a0f7a1f22d455bfa28346909cd5135fcf43ae
R. Stanley Hum <r.stanley.hum@gmail.com>
2018-10-05 07:14:19 -0400

    changed icon, added integrated splash page to android; removed widget splash page

--- lib/widgets/material_base.dart
+++ lib/widgets/material_base.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../constants.dart' as globals;
 import '../route_names.dart' as routeNames;
 
 import 'base_view.dart';
-import 'splash_view.dart';
 import 'disclaimer_view.dart';
 
-class MaterialBase extends StatelessWidget {
+class MaterialBase extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: globals.materialAppTitle,
-        theme: ThemeData(
-          primarySwatch: globals.themeColor,
-        ),
-        home: SplashView(),
-        routes: <String, WidgetBuilder>{
-          routeNames.DisclaimerViewRoute: (context) => DisclaimerView(),
-          routeNames.BaseViewRoute: (context) => BaseView(),
-        },
-      );
+  _MaterialBaseState createState() => _MaterialBaseState();
+}
+
+class _MaterialBaseState extends State<MaterialBase> {
+  bool disclaimerState;
+
+  @override
+  void initState() {
+    super.initState();
+    disclaimerState = false;
+    _getDisclaimerState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: globals.materialAppTitle,
+      theme: ThemeData(
+        primarySwatch: globals.themeColor,
+      ),
+      home: (disclaimerState == null)
+          ? DisclaimerView()
+          : (disclaimerState) ? BaseView() : DisclaimerView(),
+      routes: <String, WidgetBuilder>{
+        routeNames.DisclaimerViewRoute: (context) => DisclaimerView(),
+        routeNames.BaseViewRoute: (context) => BaseView(),
+      },
+    );
+  }
+
+  void _getDisclaimerState() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      disclaimerState = prefs.getBool(globals.disclaimerStateKey) ?? false;
+    });
+  }
 }
